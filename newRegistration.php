bae9bb85414f7136ea8d90be3b6675555df486d1
david Moreno <dmoreno757@gmail.com>
2018-04-05 18:30:21 -0700

    fixed

--- newRegistration.php
+++ newRegistration.php
@@ -4,7 +4,7 @@
     if (isset($_REQUEST['submit']) != '') {
         $sqlReg = "INSERT INTO userlogin(Name_First, Name_Last, Email, UserName, Password)
         VALUES ('".$_REQUEST['firstName']."', '".$_REQUEST['lastName']."', '".$_REQUEST['email']."', '".$_REQUEST['userName']."','".$_REQUEST['password']."')";
-    $res = mysqli_query($sql);
+    $res = mysqli_query($link, $sqlReg);
     $resultReg = $link->query($sqlReg);
     if ($resultReg) {
         echo "New Record Recorded";
