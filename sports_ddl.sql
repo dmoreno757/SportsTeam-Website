011aaf101b900b511bfe91afd61716e1d099093b
mohammed-117 <36322781+mohammed-117@users.noreply.github.com>
2018-05-01 18:22:50 -0700

    update to ddl by Mohammed

--- sports_ddl.sql
+++ sports_ddl.sql
@@ -96,53 +96,39 @@ INSERT INTO LeagueTeam VALUES
 
 
 
-CREATE TABLE GP_TeamA
+CREATE TABLE GP_Team
 (
-  TeamID_A  INTEGER UNSIGNED  NOT NULL,
-  GameRound INTEGER    UNSIGNED  NOT NULL,
-  TeamAPoints   VARCHAR(100),
-  
-  FOREIGN KEY (TeamID_A) REFERENCES LeagueTeam(TeamID) ON DELETE CASCADE
-);
+GameRound INTEGER UNSIGNED NOT NULL,
 
-GRANT         Select                 ON GP_TeamA TO 'Observer'         @'localhost';
-GRANT Insert, Select, Update, Delete ON GP_TeamA TO 'Users'            @'localhost';
-GRANT Insert, Select, Update, Delete ON GP_TeamA TO 'Executive Manager'@'localhost';
+TeamID_A  INTEGER UNSIGNED NOT NULL,
+TeamAPoints   VARCHAR(100),
 
-INSERT INTO GP_TeamA VALUES
-('1','1','50'),
-('3','1','40'),
-('5','1','70'),
-('7','1','55'),
-('1','2','70'),
-('6','2','50'),
-('1','3','20');
-
-
-
-
-
-
-CREATE TABLE GP_TeamB
-(
 TeamID_B INTEGER UNSIGNED NOT NULL,
-GameRound INTEGER    UNSIGNED  NOT NULL,
 TeamBPoints VARCHAR(100),
+
+DateTracker CHAR(10),
+Months   VARCHAR(100),
+Days     VARCHAR(100),
+Years    CHAR(10),
+
+FOREIGN KEY (TeamID_A) REFERENCES LeagueTeam(TeamID) ON DELETE CASCADE,
 FOREIGN KEY (TeamID_B) REFERENCES LeagueTeam(TeamID) ON DELETE CASCADE
 );
 
-GRANT         Select                 ON GP_TeamB TO 'Observer'         @'localhost';
-GRANT Insert, Select, Update, Delete ON GP_TeamB TO 'Users'            @'localhost';
-GRANT Insert, Select, Update, Delete ON GP_TeamB TO 'Executive Manager'@'localhost';
-   
-INSERT INTO GP_TeamB VALUES
-('2','1','20'),
-('4','1','35'),
-('6','1','20'),
-('8','1','25'),
-('4','2','35'),
-('7','2','30'),
-('6','3','15');
+INSERT INTO GP_Team VALUES
+('1','1','50','2','20','1','SAT','7th','2018'),
+('1','3','35','4','40','2','SUN','8th','2018'),
+('1','5','70','6','20','3','SAT','14th','2018'),
+('1','7','55','8','25','4','SUN','15th','2018'),
+('2','1','70','4','35','5','SAT','21st','2018'),
+('2','6','50','7','30','6','SUN','22nd','2018'),
+('3','1','20','6','15','7','FRI','27th','2018');
+
+GRANT         Select                 ON GP_Team TO 'Observer'         @'localhost';
+GRANT Insert, Select, Update, Delete ON GP_Team TO 'Users'            @'localhost';
+GRANT Insert, Select, Update, Delete ON GP_Team TO 'Executive Manager'@'localhost';
+
+
 
 
 
@@ -169,19 +155,47 @@ GRANT         Select                 ON TeamRoster TO 'Observer'         @'local
 GRANT Insert, Select, Update, Delete ON TeamRoster TO 'Users'            @'localhost';
 GRANT Insert, Select, Update, Delete ON TeamRoster TO 'Executive Manager'@'localhost';
 
-INSERT INTO TeamRoster VALUES
+
+INSERT INTO TeamRoster VALUES 
 ('100', 'Donald',               'Duck',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
 ('101', 'Daisy',                'Duck',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
 ('102', 'Mickey',               'Mouse',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
 ('103', 'Pluto',                'Dog',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
 
-('104', 'Scrooge',              'McDuck',  '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
-('105', 'Huebert (Huey)',       'Duck',    '1110 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
-('106', 'Deuteronomy (Dewey)',  'Duck',    '1110 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
-('107', 'Louie',                'Duck',    '1110 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
-('108', 'Phooey',               'Duck',    '1-1 Maihama Urayasu',     'Chiba Prefecture',   'Disney Tokyo',  'Japan',   NULL),
+('104', 'Joe',               'Luck',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('105', 'Jason',                'Sparks',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('106', 'Mickey',               'Johnson',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('107', 'Phil',                'Pooper',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
 
-('109', 'Della',                'Duck',    '77700 Boulevard du Parc', 'Coupvray',           'Disney Paris',  'France',  NULL);
+('108', 'Patrick',               'Smith',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('109', 'Will',                'Smith',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('110', 'Mike',               'Tran',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('111', 'Sandeep',                'Rademi',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+
+('112', 'Captian',               'America',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('113', 'Connie',                'Sparks',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('114', 'Mark',               'Zuckerberg',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('115', 'Steve',                'Jobs',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+
+('116', 'William',               'Miller',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('117', 'Troy',                'Stephens',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('118', 'Bob',               'Martin',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('119', 'Steve',                'Jackson',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+
+('120', 'Fred',               'Douglas',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('121', 'Jessie',                'Eisenberg',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('122', 'Jon',               'Stewart',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('123', 'Joe',                'Wilson',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+
+('124', 'Micheal',               'Jackson',    '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('125', 'Jennifer',                'Smith',    '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('126', 'Vanessa',               'Lopez',   '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+('127', 'Mario',                'Cart',     '1313 S. Harbor Blvd.',    'Anaheim',            'CA',            'USA',     '92808-3232'),
+
+('128', 'Scrooge',              'McDuck',  '1180 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('129', 'Louie',                'Duck',    '1110 Seven Seas Dr.',     'Lake Buena Vista',   'FL',            'USA',     '32830'),
+('130', 'Phooey',               'Duck',    '1-1 Maihama Urayasu',     'Chiba Prefecture',   'Disney Tokyo',  'Japan',   NULL),
+('131', 'Della',                'Duck',    '77700 Boulevard du Parc', 'Coupvray',           'Disney Paris',  'France',  NULL);
 
 
 
@@ -197,6 +211,7 @@ CREATE TABLE Statistics
   Points            TINYINT    UNSIGNED  DEFAULT 0,
   Assists           TINYINT    UNSIGNED  DEFAULT 0,
   Rebounds          TINYINT    UNSIGNED  DEFAULT 0,
+  TotalGames        INTEGER    UNSIGNED  NOT NULL,
 
   FOREIGN KEY (Player) REFERENCES TeamRoster(ID) ON DELETE CASCADE,
 
@@ -209,9 +224,42 @@ GRANT Insert, Select, Update, Delete ON Statistics TO 'Users'            @'local
 GRANT Insert, Select, Update, Delete ON Statistics TO 'Executive Manager'@'localhost';
 
 INSERT INTO Statistics VALUES
-('17', '100', '35', '12', '47', '11', '21'),
-('18', '102', '13', '22', '13', '01', '03'),
-('19', '103', '10', '00', '18', '02', '04'),
-('20', '107', '02', '45', '09', '01', '02'),
-('21', '102', '15', '39', '26', '03', '07'),
-('22', '100', '29', '47', '27', '09', '08');
+('10', '100', '35', '12', '40', '11', '21','3'),
+('11', '101', '13', '22', '50', '01', '03','3'),
+('12', '102', '10', '00', '10', '02', '04','3'),
+('13', '103', '29', '47', '40', '09', '08','3'),
+
+('14', '104', '35', '12', '5', '11', '21','1'),
+('15', '105', '13', '22', '7', '01', '03','1'),
+('16', '106', '10', '00', '3', '02', '04','1'),
+('17', '107', '29', '47', '5', '09', '08','1'),
+
+('18', '108', '35', '12', '18', '11', '21','1'),
+('19', '109', '13', '22', '12', '01', '03','1'),
+('20', '110', '10', '00', '3', '02', '04','1'),
+('21', '111', '29', '47', '2', '09', '08','1'),
+
+('22', '112', '35', '12', '30', '11', '21','2'),
+('23', '113', '13', '22', '15', '01', '03','2'),
+('24', '114', '10', '00', '20', '02', '04','2'),
+('25', '115', '29', '47', '10', '09', '08','2'),
+
+('26', '116', '35', '12', '5', '11', '21','1'),
+('27', '117', '13', '22', '5', '01', '03','1'),
+('28', '118', '10', '00', '8', '02', '04','1'),
+('29', '119', '29', '47', '2', '09', '08','1'),
+
+('30', '120', '35', '12', '20', '11', '21','3'),
+('31', '121', '13', '22', '30', '01', '03','3'),
+('32', '122', '10', '00', '30', '02', '04','3'),
+('33', '123', '29', '47', '55', '09', '08','3'),
+
+('34', '124', '35', '12', '15', '11', '21','2'),
+('35', '125', '13', '22', '15', '01', '03','2'),
+('36', '126', '10', '00', '30', '02', '04','2'),
+('37', '127', '29', '47', '25', '09', '08','2'),
+
+('38', '128', '35', '12', '5', '11', '21','1'),
+('39', '129', '13', '22', '10', '01', '03','1'),
+('40', '130', '10', '00', '5', '02', '04','1'),
+('41', '131', '29', '47', '5', '09', '08','1');
